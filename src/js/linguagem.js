9984edf6dbdae8ebec01436b59195b885c7ad728
Sound Line <168299407+SoundLine01@users.noreply.github.com>
2024-06-07 06:54:33 -0300

    add src folder

---
+++ src/js/linguagem.js
@@ -0,0 +1,66 @@
+/*const traducoes = {
+    nav: {
+        funcionalidades: 'Functionalities',
+        planos: 'Plans',
+        sobreherois: 'About the heroes',
+        entrar: 'Sign in',
+        cadastro: 'Register'
+    }
+    desta
+}
+
+console.log(traducoes.nav.funcionalidades)
+
+const navegacao = document.querySelectorAll('.nav_item');
+
+navegacaoLinks.forEach( (link) =>{
+    if(link.innerHTML == 'Funcionalidades'){
+        link.innerHTML = traducoes.nav.funcionalidades;
+    }else if(link.innerHTML =="Planos"){
+        link.innerHTML = traducoes.nav.planos;
+    }else if(link.innerHTML == "Sobre os Herois"){
+        link.innerHTML = traducoes.nav.sobreherois;
+    }else if(link.innerHTML == "Entrar"){
+        link.innerHTML = traducoes.nav.entrar;
+    }else if(link.innerHTML == "Cadastro"){
+        link.innerHTML = traducoes.nav.cadastro;
+    }
+})*/
+
+/*const languageBtn = document.getElementById('language-btn');
+const contentElements = document.querySelectorAll('[data-lang]');  // Target elements with data-lang attribute
+
+const languages = {
+  'pt-br': {
+    'title': 'SOUND LINE',
+    'hero_title': 'Deixe sua imaginação voar com nossos audiobooks. Explore, ouça, apaixone-se.',
+    'hero_msg_h3': 'Primeira vez? Faça um teste grátis!',
+    'hero_msg_h5': 'Experimente nossos audiobooks gratuitamente.',
+    'text_h3': 'Confira alguns dos nossos livros disponivéis:',
+    'apresentacao_p': 'Sua plataforma de Audiodramas favorita!',
+    'footer_title': 'E aí, pronto(a) pra começar?',
+    'footer_btn': 'Vamos nessa!',
+    'footer_nav_home': 'Home',
+    'footer_nav_sobre': 'Sobre Nossos Heróis',
+    'footer_nav_fale': 'Fale Conosco',
+    'footer_nav_planos': 'Planos',
+    'footer_item_text': 'Fique por dentro de tudo da Sound line, siga-nos nas redes sociais e conheça novas promoções.',
+    'direitos_text': '© Copyright 2024 - Todos os Direitos Reservados.'
+  },
+  'en': {  // Add English translations here
+    'title': 'SOUND LINE',
+    'hero_title': 'Let your imagination soar with our audiobooks. Explore, listen, fall in love.',
+    'hero_msg_h3': 'First time? Take a free trial!',
+    'hero_msg_h5': 'Try our audiobooks for free',
+    'text_h3': 'Check out some of our available books:',
+    'apresentacao_p': 'Your favorite audiobook platform!',
+    'footer_title': 'Ready to start?',
+    'footer_btn': 'Let\'s go!',
+    'footer_nav_home': 'Home',
+    'footer_nav_sobre': 'About Our Heroes',
+    'footer_nav_fale': 'Talk to Us',
+    'footer_nav_planos': 'Plans',
+    'footer_item_text': 'Stay up to date with everything from Sound line, follow us on social media and find out about new promotions.',
+    'direitos_text': '© Copyright 2024 - All Rights Reserved.'
+    },
+}*/
